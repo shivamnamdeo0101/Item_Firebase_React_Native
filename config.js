65a0a1493ad130cfeb57c7abab2b7c1a3b378eef
Shivam Namdeo <52598179+ShivamNamdeo@users.noreply.github.com>
2020-06-18 14:33:52 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export const  firebaseConfig = {
+    apiKey: "AIzaSyBaqet-UGSBimhzCJ4lgZJGHvfhnfA7OvI",
+    authDomain: "first-99ba6.firebaseapp.com",
+    databaseURL: "https://first-99ba6.firebaseio.com",
+    projectId: "first-99ba6",
+    storageBucket: "first-99ba6.appspot.com",
+    messagingSenderId: "441639648090",
+    appId: "1:441639648090:web:1207f7842a0db1fe1b5163",
+    measurementId: "G-69G8NL88Y8"
+  };
